34b5123bac3d24b247ec3fbb5317bae29c54c4a8
Walner <walner1borbon@gmail.com>
2018-05-13 15:54:01 -0600

    Cambio en recibir Tipo de Documento
    Antes recibia numeros, ahora recibe text FE NC ND TE CCE CPCE RCE

--- api/contrib/signXML/sign.php
+++ api/contrib/signXML/sign.php
@@ -1,27 +1,53 @@
 <?php
 
+function signFE() {
+    require 'Firmadohaciendacr.php';
+    modules_loader("files");
+    $p12Url = filesGetUrl(params_get('p12Url'));
+    $pinP12 = params_get('pinP12');
+    $inXml = params_get('inXml');
+    $tipoDoc = params_get('tipodoc');
+    $tipoDocumento;
+    switch ($tipoDoc) {
+        case 'FE': //Factura Electronica
+            $tipoDocumento = "01";
+            break;
+        case 'ND': // Nota de Debito
+            $tipoDocumento = "02";
+            break;
+        case 'NC': // Nota de Credito
+            $tipoDocumento = "03";
+            break;
+        case 'TE': // Tiquete Electronico
+            $tipoDocumento = "04";
+            break;
+        case 'CCE': // Confirmacion Comprabante Electronico
+            $tipoDocumento = "05";
+            break;
+        case 'CPCE': // Confirmacion Parcial Comprbante Electronico
+            $tipoDocumento = "06";
+            break;
+        case 'RCE': // Rechazo Comprobante Electronico
+            $tipoDocumento = "07";
+            break;
+    }
 
+    $fac = new Firmadocr();
+    //$inXmlUrl debe de ser en Base64 
+    //$p12Url es un downloadcode previamente suministrado al subir el certificado en el modulo fileUploader -> subir_certif
+    //Tipo es el tipo de documento 
+    // 01 FE
+    //02 ND
+    //03 NC
+    //04 TE
+    //05 06 07 Mensaje Receptor
 
+    $returnFile = $fac->firmar($p12Url, $pinP12, $inXml, $tipoDocumento);
+    $arrayResp = array(
+        "xmlFirmado" => $returnFile
+    );
 
-function signFE(){
-        require 'Firmadohaciendacr.php';
-        modules_loader("files");        
-        $p12Url= filesGetUrl(params_get('p12Url'));
-        $pinP12= params_get('pinP12');
-        $inXml= params_get('inXml');        
-        $tipodoc=params_get('tipodoc');
-        
-        $fac = new Firmadocr();        
-        //$inXmlUrl debe de ser en Base64 
-        //$p12Url es un downloadcode previamente suministrado al subir el certificado en el modulo fileUploader -> subir_certif
-        //Tipo es el tipo de documento 
-        // 01 FE
-        //02 ND
-        //03 NC
-        //04 TE
-        //05 Mensaje Receptor
-        $returnFile=$fac->firmar($p12Url, $pinP12,$inXml,$tipodoc );
-        
-        return $returnFile;
+    return $arrayResp;
 }
+
 ?>
