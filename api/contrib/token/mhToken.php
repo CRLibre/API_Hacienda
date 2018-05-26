5b738acc92626a1097b7ae633290492968161f22
Walner <walner1borbon@gmail.com>
2018-05-26 15:44:47 -0600

    validacion de datos enviados
    Modificado por Lizandro

--- api/contrib/token/mhToken.php
+++ api/contrib/token/mhToken.php
@@ -12,22 +12,59 @@ function token(){
 	//Get Data from Post
 	if (params_get("grant_type")=="password") {
 		
-		$data = array(
-				    'client_id' => params_get("client_id"),
-				    'client_secret' => params_get("client_secret"),
-				    'grant_type' => params_get("grant_type"),
-				    'username' => params_get("username"),
-				    'password' => params_get("password")     
-					);
-	} 
-	else if (params_get("grant_type")=="refresh_token") {
-			$data = array(
-					    'client_id' => params_get("client_id"),
-					    'client_secret' => params_get("client_secret"),
-					    'grant_type' => params_get("grant_type"),
-					    'refresh_token' =>  params_get("refresh_token")
-			);
+		$client_id 		= params_get("client_id");
+		$client_secret 	= params_get("client_secret");
+		$grant_type 	= params_get("grant_type");
+		$username 		= params_get("username");
+		$password 		= params_get("password");
+		
+		//Validation
+		if($client_id == ''){
+			return "El parametro Client ID es requerido";	
 		}
+		if($grant_type == ''){
+			return "El parametro Grant Type es requerido";	
+		}
+		if($username == ''){
+			return "El parametro Username es requerido";	
+		}
+		if($password == ''){
+			return "El parametro Password es requerido";	
+		}
+				
+		$data = array(
+				    'client_id' 	=> $client_id,
+				    'client_secret' => $client_secret,
+				    'grant_type' 	=> $grant_type,
+				    'username' 		=> $username,
+				    'password' 		=> $password     
+					);
+	} else if (params_get("grant_type")=="refresh_token") {
+
+		$client_id 		= params_get("client_id");
+		$client_secret 	= params_get("client_secret");
+		$grant_type 	= params_get("grant_type");
+		$refresh_token 	= params_get("refresh_token");
+		
+		//Validation
+		if($client_id == ''){
+			return "El parametro Client ID es requerido";	
+		}
+		
+		if($grant_type == ''){
+			return "El parametro Grant Type es requerido";	
+		}
+		if($refresh_token == ''){
+			return "El parametro Refresh Token es requerido";	
+		}
+
+		$data = array(
+					    'client_id' 	=> $client_id,
+					    'client_secret' => $client_secret,
+					    'grant_type' 	=> $grant_type,
+					    'refresh_token' => $refresh_token
+			);
+	}
 
 	//Making the options
 		$options = array(
