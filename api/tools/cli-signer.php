a81d0c17fc51b5a322a6b00b1edd861148f385ac
schweini <schweini@gmail.com>
2018-05-29 12:44:18 -0600

    usar '-' para usar STDIN/STDOUT
    (no probado todavia)

--- api/tools/cli-signer.php
+++ api/tools/cli-signer.php
@@ -14,8 +14,13 @@
         $cert = $argv[1];
         $pin = $argv[2];
         $xmlIn = $argv[3];
-
         $xmlOut = $argv[4];
+
+        if ( $xmlIn == '-' )
+        {
+                $xmlIn = "php://stdin";
+        }
+
         #$doctype = $argv[5];
 
         if ( ! file_exists( $xmlIn ) )
@@ -70,7 +75,7 @@
         if ( $doctype != '01' and $doctype != '02' and $doctyp != '03' and $doctype != '04' and $doctype != '05'  )
         {
                 #die("Usage: cli-signer.php <archivo.p12> <claveP12> <xml_sin_firmar> <tipodoc>\n");
-                die("Usage: cli-signer.php <archivo.p12> <claveP12> <xml_sin_firma> <xml_con_firma>\n");
+                die("Usage: cli-signer.php <archivo.p12> <claveP12> <xml_sin_firma> <xml_con_firma>\n(xml_sin_firma y/o xml_con_firma puede ser '-' para STDIN o STDOUT, respetivamente)");
         }
 
 
@@ -83,6 +88,13 @@
 
         #echo "\n\nOUT\n\n$foo\n";
 
-        file_put_contents( $xmlOut, $foo );
+        if ( $xmlOut == '-' )
+        {
+                echo $foo;
+        }
+        else
+        {
+                file_put_contents( $xmlOut, $foo );
+        }
 
 ?>
