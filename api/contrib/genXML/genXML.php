5ae64e53ba4506d46d54505b8b51c5c4134ab802
Walner Borbon <walner1borbon@gmail.com>
2018-05-30 19:10:45 -0600

    Se aceptan parametros mas parametros
    Se aceptan parametros de impuestos, impuestos compuestos, exoneraciones y descuentos
    parametro detalle de la siguiente manera
    
    este Json debe enviarse en el parametro de detalle
    
    {
      "1": {
        "cantidad": "1",
        "unidadMedida": "sp",
        "detalle": "Impresora",
        "precioUnitario": "10000",
        "montoTotal": "1000",
        "subtotal": "9900",
        "montoTotalLinea": "12177",
        "montoDescuento": "100",
        "naturalezaDescuento": "Pronto pago",
        "impuesto": {
          "1": {
            "codigo": "01",
            "tarifa": "13",
            "monto": "1287"
          },
          "2": {
            "codigo": "02",
            "tarifa": "10",
            "monto": "990"
          }
        }
      },
      "2": {
        "cantidad": "1",
        "unidadMedida": "sp",
        "detalle": "producto",
        "precioUnitario": "10000",
        "montoTotal": "10000",
        "subtotal": "10000",
        "montoTotalLinea": "11300",
        "impuesto": {
          "1": {
            "codigo": "01",
            "tarifa": "13",
            "monto": "1300",
            "exoneracion": {
              "tipoDocumento": "01",
              "numeroDocumento": "100",
              "nombreInstitucion": "Ministerio de Hacienda",
              "fechaEmision": "2016-09-26T13:00:00+06:00",
              "montoImpuesto": "130",
              "porcentajeCompra": "10"
            }
          }
        }
      }
    }

--- api/contrib/genXML/genXML.php
+++ api/contrib/genXML/genXML.php
@@ -1,11 +1,8 @@
 <?php
-
 /* * ************************************************** */
 /* Funcion para generar XML                          */
 /* * ************************************************** */
-
 function genXMLFe() {
-
     //datos contribuyente
     $clave = params_get("clave");
     $consecutivo = params_get("consecutivo");
@@ -15,7 +12,6 @@ function genXMLFe() {
     $emisorTipoIdentif = params_get("emisor_tipo_indetif");
     $emisorNumIdentif = params_get("emisor_num_identif");
     $nombreComercial = params_get("nombre_comercial");
-
     $emisorProv = params_get("emisor_provincia");
     $meisorCanton = params_get("emisor_canton");
     $emisorDistrito = params_get("emisor_distrito");
@@ -26,7 +22,6 @@ function genXMLFe() {
     $emisorCodPaisFax = params_get("emisor_cod_pais_fax");
     $emisorFax = params_get("emisor_fax");
     $emisorEmail = params_get("emisor_email");
-
     //datos receptor
     $receptorNombre = params_get("receptor_nombre");
     $receptorTipoIdentif = params_get("receptor_tipo_identif");
@@ -41,12 +36,10 @@ function genXMLFe() {
     $receptorCodPaisFax = params_get("receptor_cod_pais_fax");
     $receptorFax = params_get("receptor_fax");
     $receptorEmail = params_get("receptor_email");
-
     //detalles de tiquete / factura
     $condVenta = params_get("condicion_venta");
     $plazoCredito = params_get("plazo_credito");
     $medioPago = params_get("medio_pago");
-
     $codMoneda = params_get("cod_moneda");
     $tipoCambio = params_get("tipo_cambio");
     $totalServGravados = params_get("total_serv_gravados");
@@ -61,7 +54,6 @@ function genXMLFe() {
     $totalImp = params_get("total_impuestos");
     $totalComprobante = params_get("total_comprobante");
     $otros = params_get("otros");
-
     //detalles de la compra
     $detalles = json_decode(params_get("detalles"));
     grace_debug(params_get("detalles"));
@@ -122,40 +114,56 @@ function genXMLFe() {
         <MedioPago>' . $medioPago . '</MedioPago>
         <DetalleServicio>
         ';
-    //cant - unidad medida - detalle - precio unitario - monto total - subtotal - monto total linea - Monto desc -Naturaleza Desc
-
+    //cant - unidad medida - detalle - precio unitario - monto total - subtotal - monto total linea - Monto desc -Naturaleza Desc - Impuesto : Codigo / Tarifa / Monto
+    
     /* EJEMPLO DE DETALLES
       {
-      "1":["1", "Sp", "Honorarios", "100000", "100000", "100000", "100000", "1000","Pronto pago"],
-      "2":["1", "Sp", "Honorarios", "100000", "100000", "100000", "100000", "1000","Pronto pago"]
+        "1":["1","Sp","Honorarios","100000","100000","100000","100000","1000","Pronto pago",{"Imp": [{"cod": 122,"tarifa": 1,"monto": 100},{"cod": 133,"tarifa": 1,"monto": 1300}]}],
+        "2":["1","Sp","Honorarios","100000","100000","100000","100000"]
       }
      */
     $l = 1;
-
     foreach ($detalles as $d) {
-
         $xmlString .= '<LineaDetalle>
                   <NumeroLinea>' . $l . '</NumeroLinea>
-                  <Cantidad>' . $d[0] . '</Cantidad>
-                  <UnidadMedida>' . $d[1] . '</UnidadMedida>
-                  <Detalle>' . $d[2] . '</Detalle>
-                  <PrecioUnitario>' . $d[3] . '</PrecioUnitario>
-                  <MontoTotal>' . $d[4] . '</MontoTotal>';
-        if (isset($d[7]) && $d[7] != "") {
-            $xmlString .= '<MontoDescuento>' . $d[7] . '</MontoDescuento>';
+                  <Cantidad>' . $d->cantidad . '</Cantidad>
+                  <UnidadMedida>' . $d->unidadMedida . '</UnidadMedida>
+                  <Detalle>' . $d->detalle . '</Detalle>
+                  <PrecioUnitario>' . $d->precioUnitario . '</PrecioUnitario>
+                  <MontoTotal>' . $d->montoTotal . '</MontoTotal>';
+        if (isset($d->montoDescuento) && $d->montoDescuento != "") {
+            $xmlString .= '<MontoDescuento>' . $d->montoDescuento . '</MontoDescuento>';
         }
-        if (isset($d[8]) && $d[8] != "") {
-            $xmlString .= '<NaturalezaDescuento>' . $d[8] . '</NaturalezaDescuento>';
+        if (isset($d->naturalezaDescuento) && $d->naturalezaDescuento != "") {
+            $xmlString .= '<NaturalezaDescuento>' . $d->naturalezaDescuento . '</NaturalezaDescuento>';
         }
-        $xmlString .= '<SubTotal>' . $d[5] . '</SubTotal>
-        <MontoTotalLinea>' . $d[6] . '</MontoTotalLinea>';
+        if (isset($d->impuesto) && $d->impuesto != "") {
+            foreach($d->impuesto as $i)
+            {
+                $xmlString .= '<Impuesto>
+                <Codigo>' . $i->codigo . '</Codigo>
+                <Tarifa>' . $i->tarifa . '</Tarifa>
+                <Monto>' . $i->monto . '</Monto>';
+                if(isset($i->exoneracion) && $i->exoneracion!=""){
+                    $xmlString .= '
+                    <Exoneracion>
+                        <TipoDocumento>' . $i->exoneracion->tipoDocumento . '</TipoDocumento>
+                        <NumeroDocumento>' . $i->exoneracion->numeroDocumento . '</NumeroDocumento>
+                        <NombreInstitucion>' . $i->exoneracion->nombreInstitucion . '</NombreInstitucion>
+                        <FechaEmision>' . $i->exoneracion->fechaEmision . '</FechaEmision>
+                        <MontoImpuesto>' . $i->exoneracion->montoImpuesto . '</MontoImpuesto>
+                        <PorcentajeCompra>' . $i->exoneracion->porcentajeCompra . '</PorcentajeCompra>
+                    </Exoneracion>';
+                }
 
-
+                $xmlString .= '</Impuesto>';
+            }
+        }
+        $xmlString .= '<SubTotal>' . $d->subtotal . '</SubTotal>
+        <MontoTotalLinea>' . $d->montoTotalLinea . '</MontoTotalLinea>';
         $xmlString .= '</LineaDetalle>';
-
         $l++;
     }
-
     $xmlString .= '</DetalleServicio>
         <ResumenFactura>
         <CodigoMoneda>' . $codMoneda . '</CodigoMoneda>
@@ -180,18 +188,13 @@ function genXMLFe() {
         <OtroTexto>' . $otros . '</OtroTexto>
         </Otros>
         </FacturaElectronica>';
-
-
     $arrayResp = array(
         "clave" => $clave,
         "xml" => base64_encode($xmlString)
     );
-    return $arrayResp;
+    return $xmlString;
 }
-
 function genXMLNC() {
-
-
     //datos contribuyente
     $clave = params_get("clave");
     $consecutivo = params_get("consecutivo");
@@ -201,7 +204,6 @@ function genXMLNC() {
     $emisorTipoIdentif = params_get("emisor_tipo_indetif");
     $emisorNumIdentif = params_get("emisor_num_identif");
     $nombreComercial = params_get("nombre_comercial");
-
     $emisorProv = params_get("emisor_provincia");
     $meisorCanton = params_get("emisor_canton");
     $emisorDistrito = params_get("emisor_distrito");
@@ -212,7 +214,6 @@ function genXMLNC() {
     $emisorCodPaisFax = params_get("emisor_cod_pais_fax");
     $emisorFax = params_get("emisor_fax");
     $emisorEmail = params_get("emisor_email");
-
     //datos receptor
     $receptorNombre = params_get("receptor_nombre");
     $receptorTipoIdentif = params_get("receptor_tipo_identif");
@@ -227,12 +228,10 @@ function genXMLNC() {
     $receptorCodPaisFax = params_get("receptor_cod_pais_fax");
     $receptorFax = params_get("receptor_fax");
     $receptorEmail = params_get("receptor_email");
-
     //detalles de tiquete / factura
     $condVenta = params_get("condicion_venta");
     $plazoCredito = params_get("plazo_credito");
     $medioPago = params_get("medio_pago");
-
     $codMoneda = params_get("cod_moneda");
     $tipoCambio = params_get("tipo_cambio");
     $totalServGravados = params_get("total_serv_gravados");
@@ -252,10 +251,9 @@ function genXMLNC() {
     $infoRefeFechaEmision = params_get("infoRefeFechaEmision");
     $infoRefeCodigo = params_get("infoRefeCodigo");
     $infoRefeRazon = params_get("infoRefeRazon");
-
     //detalles de la compra
     $detalles = json_decode(params_get("detalles"));
-
+    //return $detalles;
     $xmlString = '<?xml version = "1.0" encoding = "utf-8"
         ?>
 <NotaCreditoElectronica xmlns="https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaCreditoElectronica" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaCreditoElectronica NotaCreditoElectronica_V4.2.xsd">
@@ -316,33 +314,49 @@ function genXMLNC() {
     <DetalleServicio>';
     /* EJEMPLO DE DETALLES
       {
-      "1":["1", "Sp", "Honorarios", "100000", "100000", "100000", "100000", "1000","Pronto pago"],
-      "2":["1", "Sp", "Honorarios", "100000", "100000", "100000", "100000", "1000","Pronto pago"]
+        "1":["1","Sp","Honorarios","100000","100000","100000","100000","1000","Pronto pago",{"Imp": [{"cod": 122,"tarifa": 1,"monto": 100},{"cod": 133,"tarifa": 1,"monto": 1300}]}],
+        "2":["1","Sp","Honorarios","100000","100000","100000","100000"]
       }
      */
     $l = 1;
-
     foreach ($detalles as $d) {
-
         $xmlString .= '<LineaDetalle>
                   <NumeroLinea>' . $l . '</NumeroLinea>
-                  <Cantidad>' . $d[0] . '</Cantidad>
-                  <UnidadMedida>' . $d[1] . '</UnidadMedida>
-                  <Detalle>' . $d[2] . '</Detalle>
-                  <PrecioUnitario>' . $d[3] . '</PrecioUnitario>
-                  <MontoTotal>' . $d[4] . '</MontoTotal>';
-        if (isset($d[7]) && $d[7] != "") {
-            $xmlString .= '<MontoDescuento>' . $d[7] . '</MontoDescuento>';
+                  <Cantidad>' . $d->cantidad . '</Cantidad>
+                  <UnidadMedida>' . $d->unidadMedida . '</UnidadMedida>
+                  <Detalle>' . $d->detalle . '</Detalle>
+                  <PrecioUnitario>' . $d->precioUnitario . '</PrecioUnitario>
+                  <MontoTotal>' . $d->montoTotal . '</MontoTotal>';
+        if (isset($d->montoDescuento) && $d->montoDescuento != "") {
+            $xmlString .= '<MontoDescuento>' . $d->montoDescuento . '</MontoDescuento>';
         }
-        if (isset($d[8]) && $d[8] != "") {
-            $xmlString .= '<NaturalezaDescuento>' . $d[8] . '</NaturalezaDescuento>';
+        if (isset($d->naturalezaDescuento) && $d->naturalezaDescuento != "") {
+            $xmlString .= '<NaturalezaDescuento>' . $d->naturalezaDescuento . '</NaturalezaDescuento>';
         }
-        $xmlString .= '<SubTotal>' . $d[5] . '</SubTotal>
-        <MontoTotalLinea>' . $d[6] . '</MontoTotalLinea>';
+        if (isset($d->impuesto) && $d->impuesto != "") {
+            foreach($d->impuesto as $i)
+            {
+                $xmlString .= '<Impuesto>
+                <Codigo>' . $i->codigo . '</Codigo>
+                <Tarifa>' . $i->tarifa . '</Tarifa>
+                <Monto>' . $i->monto . '</Monto>';
+                if(isset($i->exoneracion) && $i->exoneracion!=""){
+                    $xmlString .= '<Exoneracion>
+                        <TipoDocumento>' . $i->exoneracion->tipoDocumento . '</TipoDocumento>
+                        <NumeroDocumento>' . $i->exoneracion->numeroDocumento . '</NumeroDocumento>
+                        <NombreInstitucion>' . $i->exoneracion->nombreInstitucion . '</NombreInstitucion>
+                        <FechaEmision>' . $i->exoneracion->fechaEmision . '</FechaEmision>
+                        <MontoImpuesto>' . $i->exoneracion->montoImpuesto . '</MontoImpuesto>
+                        <PorcentajeCompra>' . $i->exoneracion->porcentajeCompra . '</PorcentajeCompra>
+                    </Exoneracion>';
+                }
 
-
+                $xmlString .= '</Impuesto>';
+            }
+        }
+        $xmlString .= '<SubTotal>' . $d->subtotal . '</SubTotal>
+        <MontoTotalLinea>' . $d->montoTotalLinea . '</MontoTotalLinea>';
         $xmlString .= '</LineaDetalle>';
-
         $l++;
     }
     $xmlString .= '</DetalleServicio>
@@ -376,18 +390,13 @@ function genXMLNC() {
         <OtroTexto>' . $otros . '</OtroTexto>
     </Otros>
 </NotaCreditoElectronica>';
-
-
     $arrayResp = array(
         "clave" => $clave,
         "xml" => base64_encode($xmlString)
     );
-
     return $arrayResp;
 }
-
 function genXMLND() {
-
 //datos contribuyente
     $clave = params_get("clave");
     $consecutivo = params_get("consecutivo");
@@ -397,7 +406,6 @@ function genXMLND() {
     $emisorTipoIdentif = params_get("emisor_tipo_indetif");
     $emisorNumIdentif = params_get("emisor_num_identif");
     $nombreComercial = params_get("nombre_comercial");
-
     $emisorProv = params_get("emisor_provincia");
     $meisorCanton = params_get("emisor_canton");
     $emisorDistrito = params_get("emisor_distrito");
@@ -408,7 +416,6 @@ function genXMLND() {
     $emisorCodPaisFax = params_get("emisor_cod_pais_fax");
     $emisorFax = params_get("emisor_fax");
     $emisorEmail = params_get("emisor_email");
-
 //datos receptor
     $receptorNombre = params_get("receptor_nombre");
     $receptorTipoIdentif = params_get("receptor_tipo_identif");
@@ -423,12 +430,10 @@ function genXMLND() {
     $receptorCodPaisFax = params_get("receptor_cod_pais_fax");
     $receptorFax = params_get("receptor_fax");
     $receptorEmail = params_get("receptor_email");
-
 //detalles de tiquete / factura
     $condVenta = params_get("condicion_venta");
     $plazoCredito = params_get("plazo_credito");
     $medioPago = params_get("medio_pago");
-
     $codMoneda = params_get("cod_moneda");
     $tipoCambio = params_get("tipo_cambio");
     $totalServGravados = params_get("total_serv_gravados");
@@ -448,11 +453,8 @@ function genXMLND() {
     $infoRefeFechaEmision = params_get("infoRefeFechaEmision");
     $infoRefeCodigo = params_get("infoRefeCodigo");
     $infoRefeRazon = params_get("infoRefeRazon");
-
-
 //detalles de la compra
     $detalles = json_decode(params_get("detalles"));
-
     $xmlString = '<?xml version="1.0" encoding="utf-8"?>
 <NotaDebitoElectronica xmlns="https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaDebitoElectronica" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaDebitoElectronica NotaDebitoElectronica_V4.2.xsd">
     <Clave>' . $clave . '</Clave>
@@ -509,39 +511,54 @@ function genXMLND() {
     <PlazoCredito>' . $plazoCredito . '</PlazoCredito>
     <MedioPago>' . $medioPago . '</MedioPago>
     <DetalleServicio>';
+    
     /* EJEMPLO DE DETALLES
       {
-      "1":["1", "Sp", "Honorarios", "100000", "100000", "100000", "100000", "1000","Pronto pago"],
-      "2":["1", "Sp", "Honorarios", "100000", "100000", "100000", "100000", "1000","Pronto pago"]
+        "1":["1","Sp","Honorarios","100000","100000","100000","100000","1000","Pronto pago",{"Imp": [{"cod": 122,"tarifa": 1,"monto": 100},{"cod": 133,"tarifa": 1,"monto": 1300}]}],
+        "2":["1","Sp","Honorarios","100000","100000","100000","100000"]
       }
      */
     $l = 1;
-
     foreach ($detalles as $d) {
-
         $xmlString .= '<LineaDetalle>
                   <NumeroLinea>' . $l . '</NumeroLinea>
-                  <Cantidad>' . $d[0] . '</Cantidad>
-                  <UnidadMedida>' . $d[1] . '</UnidadMedida>
-                  <Detalle>' . $d[2] . '</Detalle>
-                  <PrecioUnitario>' . $d[3] . '</PrecioUnitario>
-                  <MontoTotal>' . $d[4] . '</MontoTotal>';
-        if (isset($d[7]) && $d[7] != "") {
-            $xmlString .= '<MontoDescuento>' . $d[7] . '</MontoDescuento>';
+                  <Cantidad>' . $d->cantidad . '</Cantidad>
+                  <UnidadMedida>' . $d->unidadMedida . '</UnidadMedida>
+                  <Detalle>' . $d->detalle . '</Detalle>
+                  <PrecioUnitario>' . $d->precioUnitario . '</PrecioUnitario>
+                  <MontoTotal>' . $d->montoTotal . '</MontoTotal>';
+        if (isset($d->montoDescuento) && $d->montoDescuento != "") {
+            $xmlString .= '<MontoDescuento>' . $d->montoDescuento . '</MontoDescuento>';
         }
-        if (isset($d[8]) && $d[8] != "") {
-            $xmlString .= '<NaturalezaDescuento>' . $d[8] . '</NaturalezaDescuento>';
+        if (isset($d->naturalezaDescuento) && $d->naturalezaDescuento != "") {
+            $xmlString .= '<NaturalezaDescuento>' . $d->naturalezaDescuento . '</NaturalezaDescuento>';
         }
-        $xmlString .= '<SubTotal>' . $d[5] . '</SubTotal>
-        <MontoTotalLinea>' . $d[6] . '</MontoTotalLinea>';
+        if (isset($d->impuesto) && $d->impuesto != "") {
+            foreach($d->impuesto as $i)
+            {
+                $xmlString .= '<Impuesto>
+                <Codigo>' . $i->codigo . '</Codigo>
+                <Tarifa>' . $i->tarifa . '</Tarifa>
+                <Monto>' . $i->monto . '</Monto>';
+                if(isset($i->exoneracion) && $i->exoneracion!=""){
+                    $xmlString .= '<Exoneracion>
+                        <TipoDocumento>' . $i->exoneracion->tipoDocumento . '</TipoDocumento>
+                        <NumeroDocumento>' . $i->exoneracion->numeroDocumento . '</NumeroDocumento>
+                        <NombreInstitucion>' . $i->exoneracion->nombreInstitucion . '</NombreInstitucion>
+                        <FechaEmision>' . $i->exoneracion->fechaEmision . '</FechaEmision>
+                        <MontoImpuesto>' . $i->exoneracion->montoImpuesto . '</MontoImpuesto>
+                        <PorcentajeCompra>' . $i->exoneracion->porcentajeCompra . '</PorcentajeCompra>
+                    </Exoneracion>';
+                }
 
-
+                $xmlString .= '</Impuesto>';
+            }
+        }
+        $xmlString .= '<SubTotal>' . $d->subtotal . '</SubTotal>
+        <MontoTotalLinea>' . $d->montoTotalLinea . '</MontoTotalLinea>';
         $xmlString .= '</LineaDetalle>';
-
         $l++;
     }
-
-
     $xmlString .= '</DetalleServicio>
     <ResumenFactura>
         <CodigoMoneda>' . $codMoneda . '</CodigoMoneda>
@@ -573,19 +590,15 @@ function genXMLND() {
         <OtroTexto>' . $otros . '</OtroTexto>
     </Otros>
 </NotaDebitoElectronica>';
-
-
     $arrayResp = array(
         "clave" => $clave,
         "xml" => base64_encode($xmlString)
     );
     return $arrayResp;
 }
-
 /* * ************************************************** */
 /* Funcion de prueba                                 */
 /* * ************************************************** */
-
 function test() {
     return "Esto es un test";
 }
