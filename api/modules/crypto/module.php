a9bd1b0dc2f1cff6ed15c761c4a15ecb575579f7
Walner Borbon <walner1borbon@gmail.com>
2018-06-06 01:21:41 -0600

    Modulo crypto para la manejo open SSL aes-256-cbc
    Este modulo se integra como parte del core para mayor seguridad de la encriptacion

---
+++ api/modules/crypto/module.php
@@ -0,0 +1,33 @@
+
+<?php
+
+function crypto_bootMeUp(){
+	// Just booting up
+}
+/**
+ * Init function
+ */
+function crypto_init(){
+	$paths = array(
+
+		array(
+			'r' => 'encrypt',
+			'action' => 'crypto_encrypt',
+			'access' => 'users_openAccess',
+			'params' => array(
+				array("key" => "textEncrypt", "def" => "", "req" => true)
+			),
+			'file' => 'crypto.php'
+		),
+		array(
+			'r' => 'desencrypt',
+			'action' => 'crypto_desencrypt',
+			'access' => 'users_openAccess',
+			'params' => array(
+				array("key" => "textDesEncrypt", "def" => "0", "req" => false)
+			),
+			'file' => 'crypto.php'
+		)
+	);
+	return $paths;
+}
