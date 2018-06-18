627edc091f28e6543194a0b296a2fc7a75ebe345
Walner <walner1borbon@gmail.com>
2018-06-17 21:00:30 -0600

    Se agrega makeKey
    El makeKey devuelve un key de encriptacion, se debe defirnir en settings.php

--- api/modules/crypto/module.php
+++ api/modules/crypto/module.php
@@ -1,39 +1,39 @@
 
 <?php
 
-function crypto_bootMeUp(){
-	// Just booting up
+function crypto_bootMeUp() {
+    // Just booting up
 }
+
 /**
  * Init function
  */
-function crypto_init(){
-	$paths = array(
-
-		array(
-			'r' => 'encrypt',
-			'action' => 'crypto_encrypt',
-			'access' => 'users_openAccess',
-			'params' => array(
-				array("key" => "textEncrypt", "def" => "", "req" => true)
-			),
-			'file' => 'crypto.php'
-		),
-		array(
-			'r' => 'desencrypt',
-			'action' => 'crypto_desencrypt',
-			'access' => 'users_openAccess',
-			'params' => array(
-				array("key" => "textDesEncrypt", "def" => "0", "req" => false)
-			),
-			'file' => 'crypto.php'
-		),
-		array(
-			'r' => 'makeKey',
-			'action' => 'makeKey256',
-			'access' => 'users_openAccess',
-			'file' => 'crypto.php'
-		)
-	);
-	return $paths;
+function crypto_init() {
+    $paths = array(
+        array(
+            'r' => 'encrypt',
+            'action' => 'crypto_encrypt',
+            'access' => 'users_openAccess',
+            'params' => array(
+                array("key" => "textEncrypt", "def" => "", "req" => true)
+            ),
+            'file' => 'crypto.php'
+        ),
+        array(
+            'r' => 'desencrypt',
+            'action' => 'crypto_desencrypt',
+            'access' => 'users_openAccess',
+            'params' => array(
+                array("key" => "textDesEncrypt", "def" => "0", "req" => false)
+            ),
+            'file' => 'crypto.php'
+        ),
+        array(
+            'r' => 'makeKey',
+            'action' => 'makeKey256',
+            'access' => 'users_openAccess',
+            'file' => 'crypto.php'
+        )
+    );
+    return $paths;
 }
