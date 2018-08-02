7ca8a3afa5237025456a88113d817d88ef84aff8
Walner <walner1borbon@gmail.com>
2018-08-02 12:35:06 -0600

    Check receptor en envio a hacienda
    if (params_get("recp_tipoIdentificacion") == "" or params_get("recp_numeroIdentificacion") == "")
    Si es asi no se enviara a hacienda el nodo receptor en el json
    Esto es para que si en el XML no agregaron el receptor, puedan igual enviar el JSON a hacienda

--- api/contrib/send/send.php
+++ api/contrib/send/send.php
@@ -9,43 +9,35 @@ function send() {
     } else if ($apiTo == 'api-prod') {
         $url = "https://api.comprobanteselectronicos.go.cr/recepcion/v1/recepcion/";
     }
-
-    /*
-     * Variables a enviar a Hacienda
-     * Si recp_tipoIdentificacion o recp_numeroIdentificacion
-     * estan sin nada no se le envia a hacienda
-     * en caso de que los 2 tengan datos se envian a hacienda
-     */
-
-    if (params_get("recp_tipoIdentificacion") = "" or params_get("recp_numeroIdentificacion") = "") {
+    
+    if (params_get("recp_tipoIdentificacion") == "" or params_get("recp_numeroIdentificacion") == "") {
         $datos = array(
-            'clave' => params_get('clave'),
-            'fecha' => params_get("fecha"),
-            'emisor' => array(
-                'tipoIdentificacion' => params_get("emi_tipoIdentificacion"),
-                'numeroIdentificacion' => params_get("emi_numeroIdentificacion")
-            ),
-            'comprobanteXml' => params_get("comprobanteXml")
-        );
-    } else {
-        $datos = array(
-            'clave' => params_get('clave'),
-            'fecha' => params_get("fecha"),
-            'emisor' => array(
-                'tipoIdentificacion' => params_get("emi_tipoIdentificacion"),
-                'numeroIdentificacion' => params_get("emi_numeroIdentificacion")
-            ),
-            'receptor' => array(
-                'tipoIdentificacion' => params_get("recp_tipoIdentificacion"),
-                'numeroIdentificacion' => params_get("recp_numeroIdentificacion")
-            ),
-            'comprobanteXml' => params_get("comprobanteXml")
-        );
+        'clave' => params_get('clave'),
+        'fecha' => params_get("fecha"),
+        'emisor' => array(
+            'tipoIdentificacion' => params_get("emi_tipoIdentificacion"),
+            'numeroIdentificacion' => params_get("emi_numeroIdentificacion")
+        ),
+        'comprobanteXml' => params_get("comprobanteXml")
+    );
+    }else{
+    $datos = array(
+        'clave' => params_get('clave'),
+        'fecha' => params_get("fecha"),
+        'emisor' => array(
+            'tipoIdentificacion' => params_get("emi_tipoIdentificacion"),
+            'numeroIdentificacion' => params_get("emi_numeroIdentificacion")
+        ),
+        'receptor' => array(
+            'tipoIdentificacion' => params_get("recp_tipoIdentificacion"),
+            'numeroIdentificacion' => params_get("recp_numeroIdentificacion")
+        ),
+        'comprobanteXml' => params_get("comprobanteXml")
+    );
     }
-
-
 //$datosJ= http_build_query($datos);
     $mensaje = json_encode($datos);
+    grace_debug("JSON:" . $mensaje);
     $header = array(
         'Authorization: bearer ' . params_get('token'),
         'Content-Type: application/json'
@@ -56,15 +48,26 @@ function send() {
     curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($curl, CURLOPT_POSTFIELDS, $mensaje);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     $respuesta = curl_exec($curl);
     $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    $arrayResp = array(
-        "Status" => $status,
-        "to" => $apiTo,
-        "text" => explode("\n", $respuesta)
-    );
+    $err = curl_error($curl);
     curl_close($curl);
-    return $arrayResp;
+    if ($err) {
+        $arrayResp = array(
+            "Status" => $status,
+            "to" => $apiTo,
+            "text" => $err
+        );
+        return $arrayResp;
+    } else {
+        $arrayResp = array(
+            "Status" => $status,
+            "text" => explode("\n", $respuesta)
+        );
+        return $arrayResp;
+    }
 }
 
 function sendMensaje() {
@@ -107,16 +110,26 @@ function sendMensaje() {
     curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($curl, CURLOPT_POSTFIELDS, $mensaje);
-
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     $respuesta = curl_exec($curl);
     $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    $arrayResp = array(
-        "Status" => $status,
-        "to" => $apiTo,
-        "text" => explode("\n", $respuesta)
-    );
+    $err = curl_error($curl);
     curl_close($curl);
-    return $arrayResp;
+    if ($err) {
+        $arrayResp = array(
+            "Status" => $status,
+            "to" => $apiTo,
+            "text" => $err
+        );
+        return $arrayResp;
+    } else {
+        $arrayResp = array(
+            "Status" => $status,
+            "text" => explode("\n", $respuesta)
+        );
+        return $arrayResp;
+    }
 }
 
 function sendTE() {
@@ -154,16 +167,26 @@ function sendTE() {
     curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($curl, CURLOPT_POSTFIELDS, $mensaje);
-
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     $respuesta = curl_exec($curl);
     $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    $arrayResp = array(
-        "Status" => $status,
-        "to" => $apiTo,
-        "text" => explode("\n", $respuesta)
-    );
+    $err = curl_error($curl);
     curl_close($curl);
-    return $arrayResp;
+    if ($err) {
+        $arrayResp = array(
+            "Status" => $status,
+            "to" => $apiTo,
+            "text" => $err
+        );
+        return $arrayResp;
+    } else {
+        $arrayResp = array(
+            "Status" => $status,
+            "text" => explode("\n", $respuesta)
+        );
+        return $arrayResp;
+    }
 }
 
 ?>
