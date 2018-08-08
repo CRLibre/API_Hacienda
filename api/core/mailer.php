279c050a830eb88576fe9243f893359f3050d3df
Walner <walner1borbon@gmail.com>
2018-08-07 20:17:39 -0600

    adding some documentation

--- api/core/mailer.php
+++ api/core/mailer.php
@@ -9,13 +9,27 @@
  * subject=the subject
  * message=the message
  */
+
+//## to use the funtion mail() in line 32 you need install sendmail in your unix server
+//Steps for Ubuntu linux
+//1) install sendmail
+//	sudo apt-get install sendmail
+//2) config sendmail
+//	sudo sendmailconfig
+//3) Edit hosts file
+//	sudo nano /etc/hosts
+//		127.0.0.1 localhost su_dominio.com
+//4) Restart apache
+//	sudo service apache2 restart
+
+
 function mailer_sendEmail($info){
 
 	$headers = sprintf("From: %s \r\n" .
 		"Reply-To: %s \r\n" .
 		'X-Mailer: PHP/ %s',
-		conf_get('defaultMail', 'core', 'info@nneus.com'),
-		$info['replyTo'] != '' ? $info['replyTo'] : conf_get('defaultMail', 'core', 'info@nneus.com'),
+		conf_get('defaultMail', 'core', 'info@crlibre.org'),
+		$info['replyTo'] != '' ? $info['replyTo'] : conf_get('defaultMail', 'core', 'info@crlibre.org'),
 		phpversion());
 
 	return mail($info['to'], $info['subject'], $info['message'], $headers);
