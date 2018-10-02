9e228c3ee805f32c18645d464dbbeeb98edecf36
mamm26 <mamm26@users.noreply.github.com>
2018-10-02 12:07:41 -0600

    Revert "Cambio en archivo de configuración."
    This reverts commit 20df3edae917d995b6933ae2f622242ae35685ef.

--- www/settings.php
+++ www/settings.php
@@ -9,26 +9,34 @@ global $config;
 #
 #####################################################################################
 # Database name
-$config['db']['name'] = "{dbName}";
+$config['db']['name'] = "crlibreapi";
 # Database password
-$config['db']['pwd'] = "{dbPss}";
+//$config['db']['pwd'] = "g[H6gmrn6pb1";
+$config['db']['pwd'] = 'root';
 # Database user name
-$config['db']['user'] = "{dbUser}";
+$config['db']['user'] = "root";
 # Database host
-$config['db']['host'] = "{dbHost}";
+$config['db']['host'] = "localhost";
 ##############################################################################
 #
 # Crypto Keys
 #
 ##############################################################################
-$config['crypto']['key'] = "{cryptoKey}";
-
+$config['crypto']['key'] = "LkWfgWGQ/XhSd+ML13PEJsuecTHUPs9quAWGs1fMC9o=";
+##############################################################################
+#
+# print alerts
+# false or true
+#
+##############################################################################
+$config['boot']['alert'] = "false";
 
 ##############################################################################
 #
 # Core and Modules
 #
 ##############################################################################
+
 # Paths USE TRAILING SLASHES!!!
 # By default I will assume that core modules and contrib are located in the
 # same directory as the Api, but they can be placed anywhere else
@@ -43,37 +51,24 @@ $config['core']['siteName'] = 'MySite';
 $config['core']['host'] = "mySite.com";
 
 # Time in seconds for the lifetime of a session, after this time, the user must
-# log back in
-# In case you need it, -1 allows user to stay logged in
+# log back in, if you dont want to use the session life set the value in -1, li this
+#  $config['users']['sessionLifetime'] = -1;
+
 $config['users']['sessionLifetime'] = 1000;
-
-# If you want to allow CORS requests
-$config['core']['cors'] = true;
-
-##############################################################################
-#
-# Grace
-#
-#####################################################################################
-# Where do you want me to store the logs? USE TRAILING SLASH!
-#Log path
-$config['grace']['logPath'] = "../api/errors/";
-#If save errors
-$config['grace']['errors'] = false;
-#If display errors
-$config['grace']['display'] = false;
 
 /*******************************************************************************
  * You should not need to touch anything beyond this point
  */
  
+
+
 # List of core modules
-$config['modules']['core'] = array('cala','db', 'users', 'files', 'geoloc', 'wirez', 'crypto');
+$config['modules']['core']     = array('cala','db', 'users', 'files', 'geoloc', 'wirez', 'crypto');
 # List of core modules to load always, you can overide this list
 $config['modules']['coreLoad'] = array('cala', 'db', 'users', 'crypto');
 
 # Core modules location
-$config['modules']['corePath'] = $config['modules']['coreInstall'] . "modules/";
+$config['modules']['corePath']    = $config['modules']['coreInstall'] . "modules/";
 # Contributed modules
 $config['modules']['contribPath'] = $config['modules']['coreInstall'] . "contrib/";
 # Resources such as 404 not found images and such
@@ -82,3 +77,4 @@ $config['core']['resourcesPath'] = $config['modules']['coreInstall'] . 'resource
 # Location to upload files, USE TRAILING SLASH!!
 # Each user will have its own directory within this path
 $config['files']['basePath'] = $config['modules']['coreInstall'] . 'files/';
+
