c424a0a27da9ebcd8772212f68b4f7fcdf6084c7
Miguel Madrigal <43477436+mamm26@users.noreply.github.com>
2018-10-20 11:18:03 -0600

    Solucionado un problema al obtener datos de POST sin procesar. (#48)
    * Ejemplo cuando los datos son envíados por json.
    
    Closes: #45

--- www/api.php
+++ www/api.php
@@ -32,19 +32,36 @@ header('Access-Control-Allow-Methods: GET, PUT, POST');
 
 //print_r($argv);
 
-if(isset($_GET['w'])){
-	params_set('w', $_GET);
-}elseif(isset($_POST['w'])){
-	params_set('w', $_POST);
-}elseif(isset($_PUT)){
-	params_set('w', $_PUT);
-}elseif(isset($argv)){
+if (isset($_GET['w']))
+    params_set('w', $_GET);
+else if (isset($_POST['w']))
+    params_set('w', $_POST);
+else if (isset($_PUT))
+    params_set('w', $_PUT);
+else if (file_get_contents("php://input") !== null)
+{
+    $content = file_get_contents("php://input");
+    if (is_string($content) && is_array(json_decode($content, true)) && (json_last_error() == JSON_ERROR_NONE))
+    {
+        $datas = json_decode($content, true);
+        if (isset($datas['w']))
+            params_set('w', $datas);
+    }
+    else
+        die("La informacion json enviada contiene errores.");
+}
+else if (isset($argv))
+{
     grace_debug("Booting from cli...");
-    foreach($argv as $r){
+
+    foreach ($argv as $r)
+    {
        list($key, $val) = explode("=", $r);
-        $_POST[$key] = $val;   
+        $_POST[$key] = $val;
         grace_debug("Adding to post: $key => $val");
     }
-	params_set('w', $_POST); 
+
+    params_set('w', $_POST); 
 }
+
 $r = boot_itUp();
