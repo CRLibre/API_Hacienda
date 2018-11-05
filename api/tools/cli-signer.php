b53c7d99abbff04e7e5d0cae533870b339809129
Miguel Madrigal <mig.madrigal26@gmail.com>
2018-11-04 19:20:21 -0600

    Añadido el encabezado de copyright (#62)

--- api/tools/cli-signer.php
+++ api/tools/cli-signer.php
@@ -2,14 +2,26 @@
 
 <?php
 /*
- # Copyright 2018 Moritz von Schweinitz
- # Published under the GNU Affero General Public License
- # https://www.gnu.org/licenses/agpl-3.0.en.html
- # For the project https://github.com/CRLibre/API_Hacienda
+ * Copyright (C) 2018 CRLibre <https://crlibre.org>
+ * Copyright (C) 2018 Moritz von Schweinitz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
  # TODOs:
  # - if no xmlIN and/or xmlOut arguments are given, read/write to STDIN/STDOUT.
-*/
+
  date_default_timezone_set("America/Costa_Rica");
 
  include __DIR__.'/../contrib/signXML/Firmadohaciendacr.php';
--- recursos/creaMAC.php
+++ recursos/creaMAC.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ * Copyright (C) 2018 CRLibre <https://crlibre.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 $xmlString = '<?xml version="1.0" encoding="utf-8"?>
 <MensajeReceptor xmlns="https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/mensajeReceptor" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/mensajeReceptor https://tribunet.hacienda.go.cr/docs/esquemas/2016/v4.2/MensajeReceptor_4.2.xsd">
         <Clave>50630041800011538077800100001010000000269183168485</Clave>
