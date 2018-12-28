814f7008078c0c102d0b39c7a1985186f9f7533d
Manuel Prado <manuel@igeniuscr.com>
2018-12-27 19:54:02 -0600

    Se devuelve el cambio en los include once

--- api/core/boot.php
+++ api/core/boot.php
@@ -19,12 +19,13 @@
 /**
  * @page
  */
-include_once 'checks.php';
-include_once 'params.php';
-include_once 'modules.php';
-include_once 'conf.php';
-include_once 'grace.php';
-include_once 'tools.php';
+include_once("checks.php");
+include_once("grace.php");
+include_once("params.php");
+include_once("modules.php");
+include_once("conf.php");
+include_once("tools.php");
+
 
 /** @defgroup Constants
  *  @{
@@ -48,27 +49,27 @@ define('CLI_MODE', false);
  */
 function boot_itUp($mode = 'web')
 {
-    grace_debug('Booting up');
+    grace_debug("Booting up");
 
-    if (conf_get('alert', 'boot') == 'false') {
-        error_reporting(0);
-    } // Turn off all error reporting
+    if (conf_get('alert', 'boot') == 'false')
+        error_reporting(0); // Turn off all error reporting
 
-    // Load all core modules
-    // @todo Call the current requested module first in case it wants to change the core modules to be loaded
+    # Load all core modules
+    # @todo Call the current requested module first in case it wants to change the core modules to be loaded
     boot_loadAllCoreModules();
 
-    if ($mode != 'web') {
+    if ($mode != 'web')
+    {
         // Set mode
         conf_set('mode', 'core', 'cli');
         params_cliLoadOpts(cala_init());
     }
 
-    // Select and load the correct called module
-    // @todo if the module is core and it was already loaded, don't do it again :)
+    # Select and load the correct called module
+    # @todo if the module is core and it was already loaded, don't do it again :)
     modules_loader(params_get('w', 'cala'));
 
-    // Init this call
+    # Init this call
     return boot_initThisPath();
 }
 
@@ -81,14 +82,15 @@ function boot_initThisPath()
      *  \n (breakline) in each post request parameter, which breaks everything
      *  this issue is still under investigation
      */
-    $f = preg_replace("/[\n\r\f]+/m", '', params_get('w', 'core') . '_init');
+    $f = preg_replace("/[\n\r\f]+/m", "", params_get('w', 'core') . "_init");
 
-    if (function_exists($f)) {
-        grace_debug('Function found');
+    if (function_exists($f))
+    {
+        grace_debug("Function found");
         $response = tools_proccesPath(call_user_func($f));
-    } else {
-        $response = 'Module not found';
     }
+    else
+        $response = "Module not found";
 
     tools_reply($response);
 }
@@ -99,10 +101,11 @@ function boot_initThisPath()
  */
 function boot_loadAllCoreModules()
 {
-    grace_debug('Loading all core modules');
+    grace_debug("Loading all core modules");
 
-    foreach (conf_get('coreLoad', 'modules') as $module) {
-        grace_debug('Loading module: ' . $module);
+    foreach (conf_get('coreLoad', 'modules') as $module)
+    {
+        grace_debug("Loading module: " . $module);
         modules_loader($module);
     }
 }
