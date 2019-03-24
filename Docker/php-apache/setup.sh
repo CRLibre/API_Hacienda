a0dce10fee375652b9c81d004b43d03329553a1b
hmadrigal <hfmad@hotmail.com>
2019-03-24 11:46:59 -0600

    Trying to run scripts

--- Docker/php-apache/setup.sh
+++ Docker/php-apache/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+echo "============== BEGIN: Computing: /var/www/html/settings.php ======================"
+
 # while ! nc -z 127.0.0.1 8080; do echo sleeping; sleep 1; done;
 # echo Connected!;
-echo "============== COMPUTING DEFAULT VALUES ======================"
-
-jsonValue() { KEY=$1; num=$2; awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p ;} 
-CRLIBRE_API_HACIENDA_CRYPTO_KEY=`curl "http://localhost:80/api.php?w=crypto&r=makeKey" | jsonValue resp`;
+# jsonValue() { KEY=$1; num=$2; awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p ;} 
+# CRLIBRE_API_HACIENDA_CRYPTO_KEY=`curl "http://localhost:80/api.php?w=crypto&r=makeKey" | jsonValue resp`;
 
 # Generating settings.php 
 cat /var/www/html/settings.php.dist \
@@ -22,3 +22,4 @@ cat /var/www/html/settings.php.dist \
 # | sed "s/{dbHost}/${CRLIBRE_API_HACIENDA_DB_HOST}/g" \
 # | sed "s/{cryptoKey}/${CRLIBRE_API_HACIENDA_CRYPTO_KEY}/g" \
 # > /var/www/html/settings.php
+echo "============== END: Computing: /var/www/html/settings.php ======================"
