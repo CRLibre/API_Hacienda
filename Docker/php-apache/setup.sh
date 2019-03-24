ac299cb0d6fd1ddaf9ed90fbbe65472ccb8e0d9b
hmadrigal <hfmad@hotmail.com>
2019-03-24 12:01:09 -0600

    Trying to run scripts

--- Docker/php-apache/setup.sh
+++ Docker/php-apache/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "============== BEGIN: Computing: /var/www/html/settings.php ======================"
-
-# while ! nc -z 127.0.0.1 8080; do echo sleeping; sleep 1; done;
-# echo Connected!;
-# jsonValue() { KEY=$1; num=$2; awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p ;} 
-# CRLIBRE_API_HACIENDA_CRYPTO_KEY=`curl "http://localhost:80/api.php?w=crypto&r=makeKey" | jsonValue resp`;
+/usr/local/bin/apache2-foreground 
+while ! nc -z 127.0.0.1 8080; do echo sleeping; sleep 1; done;
+echo Connected!;
+jsonValue() { KEY=$1; num=$2; awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p ;} 
+CRLIBRE_API_HACIENDA_CRYPTO_KEY=`curl "http://localhost:80/api.php?w=crypto&r=makeKey" | jsonValue resp`;
 
 # Generating settings.php 
 cat /var/www/html/settings.php.dist \
