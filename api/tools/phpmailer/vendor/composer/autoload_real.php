a09eaf02b788857dd880e351e32f345aa10f5c50
Francisco de la Peña <fran@fran.cr>
2024-12-05 23:51:15 -0600

    Update vendored phpmailer from 6.5.0 to 6.9.3 Updated vendored composer autoloader to latest LTS This should keep autoloader compatible with PHP 5.5 yet phpmailer now supports PHP 8.4 so it is worth the bump

--- api/tools/phpmailer/vendor/composer/autoload_real.php
+++ api/tools/phpmailer/vendor/composer/autoload_real.php
@@ -13,19 +13,24 @@ class ComposerAutoloaderInit2fe2ca463dea1a5c6247a0400874ba59
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit2fe2ca463dea1a5c6247a0400874ba59', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit2fe2ca463dea1a5c6247a0400874ba59', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit2fe2ca463dea1a5c6247a0400874ba59::getInitializer($loader));
         } else {
---
+++ api/tools/phpmailer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '4.4.x-dev',
+        'version' => '4.4.9999999.9999999-dev',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c12950b9a3f93c11a0354b9823d0d5b2c5304b0b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '4.4.x-dev',
+            'version' => '4.4.9999999.9999999-dev',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c12950b9a3f93c11a0354b9823d0d5b2c5304b0b',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.3',
+            'version' => '6.9.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => '2f5c94fe7493efc213f643c23b1b1c249d40f47e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
