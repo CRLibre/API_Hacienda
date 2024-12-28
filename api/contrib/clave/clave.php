f9a5e96091d0e20c7be2a7b204b7c1c06326a02f
JeanCarlos Chavarria <jchavarr@akamai.com>
2024-12-28 09:28:21 -0600

    Improve legibility of comments

--- api/contrib/clave/clave.php
+++ api/contrib/clave/clave.php
@@ -17,7 +17,7 @@
  */
 
 // Funcion para generar clave
-/**
+/*
  * Esta funcion se puede llamar desde GET POST si se envian los siguientes parametros
  * w=clave
  * r=getClave
@@ -32,7 +32,7 @@
  * Tambien se puede llamar desde un metodo de la siguiente manera:
  * modules_loader("clave");       <-- Esta funcion importa el modulo
  * getClave($tipoDocumento="",$tipoCedula = "", $cedula = "", $situacion = "", $codigoPais = "", $consecutivo = "", $codigoSeguridad = "")  <------------ esta funcion retorna la clave
- **/
+ */
 /*
  * Tipo de comprobante o documento asociado Código
   Factura electrónica 01
