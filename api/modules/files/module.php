064ea9489657a108a5b66f2563ac3a8b1b39aced
JeanC Ch. H. <jeancarlos.chavarria19@gmail.com>
2025-01-21 15:05:10 -0600

    Update year to 2025 (#184)
    * Fix of function typo definition
    * Adding files permission in Dockerfile
    * Improve legibility of comments
    * Improve comments of dockerfile
    * Update year to 2025
    
    Signed-off-by: JeanCarlos Chavarria <jeancarlos.chavarria19@gmail.com>

--- api/modules/files/module.php
+++ api/modules/files/module.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (C) 2017-2024 CRLibre <https://crlibre.org>
+ * Copyright (C) 2017-2025 CRLibre <https://crlibre.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
