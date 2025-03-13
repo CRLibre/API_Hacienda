b6f45288f32c255b31feca146429898d3d6afd2c
Jean Chavarria <jeancarlos.chavarria19@gmail.com>
2025-03-12 23:59:19 -0600

    Issue CRLIBRE #104
    Signed-off-by: Jean Chavarria <jeancarlos.chavarria19@gmail.com>

--- api/contrib/genXML/xmlGenerator.php
+++ api/contrib/genXML/xmlGenerator.php
@@ -16,6 +16,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @deprecated
+ *
+ * @return $this
+ */
 function genXML()
 {
     $detalle                = params_get("detalle");
